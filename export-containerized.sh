39da8856c457e70bb629b9b4df3179070089b3cf
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-03-18 14:06:08 +0100

    containerized version

---
+++ export-containerized.sh
@@ -0,0 +1,18 @@
+BASEDIR=$(readlink -f $(dirname "$0") )
+ENV_FILE=${BASEDIR}/env-src
+
+[ -f ${ENV_FILE} ] || {
+    echo "Configuration file missing: ${ENV_FILE}"
+    exit 1
+}
+
+. ${ENV_FILE}
+
+TS=`date '+%Y%m%d_%H%M%S'`
+BACKUP=backup_${TS}.sql.gz
+
+docker run -i --rm \
+    naqoda/mysql-client \
+    mysqldump --protocol=tcp --compress --host=${DB_HOST} --port=${DB_PORT} --user=${DB_USER} --password=${DB_PASS} ${DB_NAME} | gzip > ${BACKUP}
+
+[ "${PIPESTATUS}" -eq "0" ] || rm ${BACKUP}
